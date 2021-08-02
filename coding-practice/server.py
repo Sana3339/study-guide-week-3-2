4c46c6c2480b15b0244336fcaf5043ffd56a7a5d
Sana <sanaahmad393@gmail.com>
2021-08-02 19:15:31 +0000

    Completed basic routes in server.py

--- coding-practice/server.py
+++ coding-practice/server.py
@@ -1,26 +1,34 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, redirect
 
 app = Flask(__name__)
 app.secret_key = "blahhhhhhhh"
 
 @app.route('/')
 def show_homepage():
-    return render_template('homepage.html')
 
-###############################
-#                             #
-# 1) Finish the routes below. #
-#                             #
-###############################
+    return render_template('homepage.html')
 
 
 @app.route('/form')
 def show_form():
-    pass
+
+    return render_template("form.html")
+
+
+@app.route('/save-name')
+def show_results():
+
+    user_name = request.form.args("name")
+
+    session["name"] = user_name
+
+    return redirect('/')
+
 
 @app.route('/results')
 def show_results():
-    pass
+
+    return render_template("results.html")
 
 
 if __name__ == "__main__":
